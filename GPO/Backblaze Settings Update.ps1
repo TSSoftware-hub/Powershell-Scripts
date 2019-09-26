3d0c2478b37c0410a7c97cd3cb4d0f08cb33cb9c
TSSoftware-hub <sergey.trukhanov@outlook.com>
2019-09-25 19:07:05 -0700

    Added a couple scripts which using to push via GPO

---
+++ GPO/Backblaze Settings Update.ps1
@@ -0,0 +1,13 @@
+﻿# Author: Sergey Trukhanov
+# Version: 1
+# Description: Changes Backblaze settings
+
+If (Test-Path "C:\Programdata\Backblaze\bzdata\bzinfo.xml") {
+    [xml]$XMLfile = Get-Content C:\Programdata\Backblaze\bzdata\bzinfo.xml
+    $node = $XMLfile.bzinfo.do_backup
+    $node.net_auto_throttle='false'
+    $node.net_throttle='70'
+    $XMLfile.Save(" C:\Programdata\Backblaze\bzdata\bzinfo.xml") 
+} else {
+    Write-Host "Backblaze is not installed!" -ForegroundColor DarkRed
+}
---
+++ GPO/WiFi-WPA2-personal-import.ps1
@@ -0,0 +1,14 @@
+﻿# Author: Sergey Trukhanov
+# Version: 1
+# Description: adds network profile to newly added devices. Currently it is WPA2-Personal profile.
+
+# Check that profile doesn't exist on a laptop
+$checkSSID = (Get-NetConnectionProfile).Name
+
+# Get all existing profiles from the folder
+if ($checkSSID -eq "netNameHere") {
+    Write-Host "netNameHere profile already exists"
+} else {
+    $XmlDirectory = "pathToFolder"
+    Get-ChildItem $XmlDirectory | Where-Object {$_.extension -eq ".xml"} | ForEach-Object {netsh wlan add profile filename=($XmlDirectory+"\"+$_.name)}
+}
